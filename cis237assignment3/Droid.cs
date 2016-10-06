1693d5f646c6e9b59091e088b025357e4c5c1781
David Hatten <dhatten87@gmail.com>
2016-10-06 17:22:05 -0400

    Added all necessary classes and interfaces that will be used in the program

---
+++ cis237assignment3/Droid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment3
+{
+    abstract class Droid : IDroid
+    {
+        protected decimal mTotalCost;
+        public decimal TotalCost
+        {
+            get
+            {
+                return mTotalCost;
+            }
+
+            set
+            {
+                mTotalCost = value;
+            }
+        }
+
+        public void CalculateTotalCost()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
